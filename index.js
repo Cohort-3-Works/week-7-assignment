fd19b38d7c75b4b8261785046efe9efd25699a1b
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-09-21 16:46:21 +0530

    wrote signup and signin endpoint

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
 require("dotenv").config();
+const { UserModel, TodoModel } = require("./db/db");
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/signup", async (req, res) => {});
+app.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
 
-app.post("/signin", async (req, res) => {});
+  const user = await userModel.create({
+    name,
+    email,
+    password,
+  });
+
+  res.status(201).json({
+    message: "User created successfully",
+  });
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const response = await userModel.findOne({
+    email,
+    password,
+  });
+  if (!response) {
+    return res.status(401).json({
+      message: "Invalid Credentials",
+    });
+  }
+
+  const token = jwt.sign(
+    { id: response._id.toString() },
+    process.env.JWT_SECRET
+  );
+
+  return res.status(200).json({
+    message: "User signed in successfully",
+    token,
+  });
+});
 
 app.post("/todos", async (req, res) => {});
 
